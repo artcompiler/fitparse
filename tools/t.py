75375bec24ee0cf2b8822e38780f4dce51a9c297
Jeff Dyer <jeffdyer@acm.org>
2020-06-10 09:44:42 -0700

    Generate JSON

--- tools/t.py
+++ tools/t.py
@@ -1,18 +1,19 @@
 from fitparse import FitFile
 
 fitfile = FitFile('./data/Test1.fit')
-
 # Get all data messages that are of type record
+print "[{"
 for record in fitfile.get_messages('record'):
 
     # Go through all the data entries in this record
     for record_data in record:
 
         # Print the records name and value (and units if it has any)
-        if record_data.units:
-            print " * %s: %s %s" % (
+        if False: # record_data.units:
+            print "%s: %s %s" % (
                 record_data.name, record_data.value, record_data.units,
             )
         else:
-            print " * %s: %s" % (record_data.name, record_data.value)
-    print
+            print "  %s: %s," % (record_data.name, record_data.value)
+    print "}, {"
+print "}]"
